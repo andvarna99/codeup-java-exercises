dd349cfdd55d2060096ad248b667b4f11fc4dead
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-02-21 14:06:10 -0600

    2/21 exercise

--- src/main/java/util/Input.java
+++ src/main/java/util/Input.java
@@ -6,36 +6,44 @@ public class Input {
 
     private Scanner scanner;
 
+//    get a scanner
     public Scanner getScanner() {
         return scanner;
     }
 
+//    get an input
     public Input() {
         scanner = new Scanner(System.in);
     }
 
+//    get a string
     public String getString(){
         return scanner.nextLine();
     }
+
+//    get a string with a prompt
     public String getString(String prompt){
         System.out.println(prompt);
+        scanner.nextLine();
         return getString();
     }
 
+//    get a boolean value
     public boolean yesNo(){
-//        System.out.println("Enter yes/y: ");
         String input = scanner.nextLine();
-        //String response = getString();
         if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y")){
             return true;
         }
         return false;
     }
+
+//    get a boolean value with a prompt
     public boolean yesNo(String prompt){
         System.out.println(prompt);
         return yesNo();
     }
 
+// get an integer between a range
     public int getInt(int min, int max){
         System.out.println("Enter a number between " + min + " and " + max + ":");
         int num = scanner.nextInt();
@@ -48,14 +56,27 @@ public class Input {
         return num;
     }
 
+//    get an integer and check if integer with try/catch
     public  int getInt(){
-        return scanner.nextInt();
+        int userInt = 0;
+        try {
+            String s = getString();
+            userInt = Integer.valueOf(s);
+        }catch(NumberFormatException e){
+            System.out.println("Enter a valid non-decimal number: ");
+            userInt = getInt();
+        }
+        return userInt;
     }
+
+//    get an integer with a prompt
     public int getInt(String prompt){
         System.out.println(prompt);
+        scanner.nextLine();
         return getInt();
     }
 
+//    get a double between a range
     public double getDouble(double min, double max){
         System.out.println("Enter a decimal between " + min + " and " + max + ":");
         double dec = scanner.nextDouble();
@@ -68,12 +89,48 @@ public class Input {
         return dec;
     }
 
+//    get a double and check if double with try/catch
     public double getDouble(){
-        return scanner.nextDouble();
+        double userDouble = 0;
+        try {
+            String s = getString();
+            userDouble = Double.valueOf(s);
+        }catch(NumberFormatException e){
+            System.out.println("Enter a valid decimal number: ");
+            userDouble = getDouble();
+        }
+        return userDouble;
     }
+
+//    get a double with a prompt
     public double getDouble(String prompt){
         System.out.println(prompt);
+        scanner.nextLine();
         return getDouble();
+    }
+
+    public int getBinary(){
+        int userBinary = 0;
+        try{
+            String s = getString();
+            userBinary = Integer.parseInt(s,2);
+        }catch(Exception e){
+            System.out.println("Enter a valid binary number: ");
+            userBinary = getBinary();
+        }
+        return userBinary;
+    }
+
+    public int getHex(){
+        int userHex = 0;
+        try{
+            String s = getString();
+            userHex = Integer.parseInt(s,16);
+        }catch(Exception e){
+            System.out.println("Enter a valid hex number: ");
+            userHex = getHex();
+        }
+        return userHex;
     }
 
 }
--- src/main/java/util/InputTest.java
+++ src/main/java/util/InputTest.java
@@ -4,6 +4,20 @@ public class InputTest {
 
     public static void main(String[] args) {
         Input userInput = new Input();
+
+        //we don't want this to crash
+        int anInt = userInput.getInt();
+        System.out.println(anInt);
+
+        double aDouble = userInput.getDouble();
+        System.out.println(aDouble);
+
+        int aBinary = userInput.getBinary();
+        System.out.println(aBinary);
+
+        int aHex = userInput.getHex();
+        System.out.println(aHex);
+
         System.out.println(userInput.getString("Enter a string: "));
         System.out.println(userInput.yesNo("Enter yes/y: "));
         System.out.println(userInput.getInt(1,10));
